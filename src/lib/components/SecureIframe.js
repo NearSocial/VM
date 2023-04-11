ca84e6d1b46f04f10d0f0149a40baccde1dbe035
Caleb Jacob <thecalebjacob@gmail.com>
2023-04-11 15:25:20 -0600

    Adding <IframeResizer /> support within <SecureIframe />

--- src/lib/components/SecureIframe.js
+++ src/lib/components/SecureIframe.js
@@ -1,13 +1,65 @@
 import React, { useCallback, useEffect, useState } from "react";
+import IframeResizer from "iframe-resizer-react";
 import { deepCopy, deepEqual } from "../data/utils";
 
 export default function SecureIframe(allProps) {
-  const { className, style, src, srcDoc, title, message, onMessage } = allProps;
-  const usedProps = { className, style, src, srcDoc, title };
+  const {
+    className,
+    id,
+    style,
+    src,
+    srcDoc,
+    title,
+    message,
+    onMessage,
+    iframeResizer,
+  } = allProps;
+
+  const usedProps = { className, id, style, src, srcDoc, title };
 
   const [loaded, setLoaded] = useState(false);
   const [prevMessage, setPrevMessage] = useState(undefined);
   const ref = React.useRef();
+
+  const returnIframeResizerProps = () => {
+    const result = {
+      ...usedProps,
+      style: style ?? { width: "1px", minWidth: "100%" },
+      checkOrigin: false,
+    };
+
+    const allIframeResizerProps =
+      typeof iframeResizer === "object" ? iframeResizer : {};
+
+    const allowedIframeResizerProps = [
+      "log",
+      "autoResize",
+      "bodyBackground",
+      "bodyMargin",
+      "bodyPadding",
+      "inPageLinks",
+      "heightCalculationMethod",
+      "maxHeight",
+      "maxWidth",
+      "minHeight",
+      "minWidth",
+      "minWidth",
+      "resizeFrom",
+      "scrolling",
+      "sizeHeight",
+      "sizeWidth",
+      "tolerance",
+      "widthCalculationMethod",
+    ];
+
+    Object.keys(allIframeResizerProps).forEach((key) => {
+      if (allowedIframeResizerProps.includes(key)) {
+        result[key] = allIframeResizerProps[key];
+      }
+    });
+
+    return result;
+  };
 
   const onMessageEvent = useCallback(
     (event) => {
@@ -36,6 +88,17 @@ export default function SecureIframe(allProps) {
   useEffect(() => {
     setLoaded(false);
   }, [src, srcDoc]);
+
+  if (iframeResizer) {
+    return (
+      <IframeResizer
+        {...returnIframeResizerProps()}
+        forwardRef={ref}
+        sandbox="allow-scripts"
+        onLoad={() => setLoaded(true)}
+      />
+    );
+  }
 
   return (
     <iframe
