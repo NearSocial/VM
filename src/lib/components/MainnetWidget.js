f609ee94ba3b7e3a9e97bc25df8d1f40768dc690
esaminu <esaminu1@icloud.com>
2023-05-26 05:34:18 +0400

    fix: context

--- src/lib/components/MainnetWidget.js
+++ src/lib/components/MainnetWidget.js
@@ -2,11 +2,11 @@ import { Widget } from "./Widget";
 import React, { createContext } from "react";
 import { useMainnetNear } from "../data/near";
 
-export const NearOverrideContext = createContext()
+export const NearOverrideContext = createContext(null)
 
 export const MainnetWidget = (props) => {
   const near = useMainnetNear();
-  console.log(near, '<<< mainnetNear context1')
+  console.log(near, '<<< mainnetNear context2')
 
   return (
     <NearOverrideContext.Provider value={near}>
