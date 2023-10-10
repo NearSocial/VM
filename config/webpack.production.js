7bfa17907cb795b8642c7460394c3c613022ca48
Matej Vukosav <MatejVukosav@users.noreply.github.com>
2023-10-10 10:26:05 +0200

    feat: enable eval source map to prod (#121)
    feat: add eval source map

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -10,7 +10,7 @@ module.exports = () => {
     //   // filename: "[name].[contenthash].bundle.js",
     //   filename: "index.js",
     // },
-    devtool: false,
+    devtool: 'eval-source-map',
     module: {
       rules: [
         // {
