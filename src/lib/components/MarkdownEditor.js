575fb428f3440b1a2f97b284c431fd349ec8f800
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2023-12-14 16:15:34 +0100

    fix: emojis new line code cleanup (#245)

--- src/lib/components/MarkdownEditor.js
+++ src/lib/components/MarkdownEditor.js
@@ -1,24 +1,9 @@
 import "./quill.snow.css";
-import React, { useEffect } from "react";
+import React from "react";
 import ReactQuill from "react-quill";
 import { sanitizePasteHtml } from "virtualized-chat";
 
 export const MarkdownEditor = (props) => {
-  useEffect(() => {
-    if (props.selectedEmoji) {
-      let newValue = "";
-      if (props.value) {
-        newValue = `${props.value.replace(/<p><br><\/p>\s*$/i, '')}${props.selectedEmoji}`;
-      } else {
-        newValue = `<p>${props.selectedEmoji}</p>`;
-      }
-      props.setValue(newValue);
-      props.resetSelectedEmoji();
-    } else if (!props.value) {
-      props.setValue("");
-    }
-  }, [props.selectedEmoji, props.value]);
-
   var toolbarOptions = ['bold', 'italic', 'underline', 'strike', { 'list': 'ordered' }, { 'list': 'bullet' }];
 
   return (
