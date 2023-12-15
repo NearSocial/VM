7e459ac0c6aa252052a7c137e5a8d28611dfcfde
Fran Domovic <93442516+frdomovic@users.noreply.github.com>
2023-12-15 13:52:23 +0100

    feat: replace ReactQuill component with plain Quill (#246)

--- src/lib/components/MarkdownEditor.js
+++ src/lib/components/MarkdownEditor.js
@@ -1,27 +1,77 @@
 import "./quill.snow.css";
-import React from "react";
-import ReactQuill from "react-quill";
+import React, { useEffect, useRef } from "react";
+
+import Quill from "quill";
 import { sanitizePasteHtml } from "virtualized-chat";
 
 export const MarkdownEditor = (props) => {
-  var toolbarOptions = ['bold', 'italic', 'underline', 'strike', { 'list': 'ordered' }, { 'list': 'bullet' }];
+  const ref = useRef(null);
+  const quillRef = useRef(null);
+  const toolbarOptions = ['bold', 'italic', 'underline', 'strike', { 'list': 'ordered' }, { 'list': 'bullet' }];
+
+  const sendMessage = () => {
+    props.handleMessageSent(quillRef.current.root.innerHTML);
+    quillRef.current.setText('');
+  }
+
+  const handleDrop = (e) => e.preventDefault();
+
+  const handlePaste = (e) => {
+    e.preventDefault();
+    const clipboardData = e.clipboardData;
+    let pastedData;
+
+    if (clipboardData.types.includes('text/html')) {
+      pastedData = clipboardData.getData('text/html');
+    } else if (clipboardData.types.includes('text/plain')) {
+      pastedData = clipboardData.getData('text/plain');
+    }
+    const processedData = sanitizePasteHtml(pastedData);
+    const selection = quillRef.current.getSelection(true);
+    quillRef.current.insertText(selection.index, processedData);
+  };
+
+  useEffect(() => {
+    if (ref.current) {
+      quillRef.current = new Quill(ref.current, {
+        modules: {
+          toolbar: toolbarOptions,
+          keyboard: {
+            bindings: {
+              enter: {
+                  key: 13,
+                  handler: sendMessage
+              }
+          },
+        }
+      },
+      theme: "snow",
+      });
+      quillRef.current.root.innerHTML = props.value;
+      quillRef.current.root.addEventListener("paste", handlePaste);
+      quillRef.current.root.addEventListener("drop", handleDrop);
+    }
+  }, []);
+
+  useEffect(() => {
+    const currentQuill = quillRef.current;
+
+    if (currentQuill) {
+      if (currentQuill.root.innerHTML !== props.value) {
+        const delta = currentQuill.clipboard.convert(props.value);
+        currentQuill.setContents(delta, "silent");
+      }
+
+      const handleTextChange = (delta, oldDelta, source) => {
+        const value = currentQuill.root.innerHTML;
+        props.setValue(value);
+      };
+
+      currentQuill.on("text-change", handleTextChange);
+    }
+  }, [quillRef.current, props.value]);
 
   return (
-    <ReactQuill
-      modules={{
-        toolbar: toolbarOptions
-      }}
-      theme="snow"
-      value={props.value}
-      onChange={(value) => {
-        const purifiedText = sanitizePasteHtml(value);
-        props.setValue(purifiedText);
-      }}
-      onKeyDown={(e) => {
-        if (e.key === "Enter" && !e.shiftKey) {
-          props.handleMessageSent();
-        }
-      }}
-    />
+      <div ref={ref} />
   );
 };
